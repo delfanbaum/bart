40c27e2c90c9362622a60b5c21e034a48402a419
Danny Elfanbaum <drelfanbaum@gmail.com>
2024-09-04 20:39:28 -0400

    Work on builder

--- src/build.rs
+++ src/build.rs
@@ -1,6 +1,12 @@
 use core::fmt;
+use std::{fmt::format, fs, path::Path};
 
 use clap::ValueEnum;
+
+use crate::{
+    document::{Document, SupportedMarkup},
+    project::BartProject,
+};
 
 #[derive(Debug, ValueEnum, Clone, Copy, PartialEq, Eq)]
 pub enum BuildTargets {
@@ -17,4 +23,52 @@ impl fmt::Display for BuildTargets {
             BuildTargets::Pdf => write!(f, "pdf"),
         }
     }
+}
+
+pub struct Builder {
+    project: Option<BartProject>,
+    docs: Vec<Document>,
+    target: BuildTargets,
+}
+
+impl Builder {
+    pub fn build(&self) {
+        let out_file_name = match &self.project {
+            Some(project) => project.name.clone(),
+            None => self.docs[0].path.to_str().unwrap().to_owned(),
+        };
+
+        let html = self.to_html();
+        match &self.target {
+            BuildTargets::Html => {
+                let out_path = Path::new(&out_file_name);
+                fs::write(out_path.with_extension("html"), html.concat())
+                    .expect("Error writing HTML")
+            }
+            BuildTargets::Word => todo!(),
+            BuildTargets::Pdf => todo!(),
+        }
+    }
+
+    /// All formats at least pitstop in HTML
+    fn to_html(&self) -> Vec<String> {
+        let mut html_strings = Vec::new();
+        for doc in self.docs.iter() {
+            let html = match doc.get_markup_language() {
+                SupportedMarkup::Markdown => markdown::to_html(&doc.read()),
+                SupportedMarkup::Text => text_to_html(&doc.read()),
+            };
+            html_strings.push(html)
+        }
+        html_strings
+    }
+}
+
+// this needs testing!
+fn text_to_html(value: &str) -> String {
+    println!("{}", value);
+    let paras: Vec<_> = value.split("\n\n").collect();
+    let html: String = paras.into_iter().map(|p| format!("<p>{p}</p>")).collect();
+    println!("{}", html);
+    html
 }
--- src/document.rs
+++ src/document.rs
@@ -1,15 +1,31 @@
-use std::path::PathBuf;
+use core::panic;
+use std::{fs, path::PathBuf};
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Document {
-    pub path: PathBuf
+    pub path: PathBuf, // TK: description, other things
+}
+
+impl Document {
+    pub fn get_markup_language(&self) -> SupportedMarkup {
+        match self.path.extension().unwrap().to_str().unwrap() {
+            "md" | "markdown" => SupportedMarkup::Markdown,
+            "txt" => SupportedMarkup::Text,
+            _ => panic!("Unsupported markup format."),
+        }
+    }
+
+    pub fn read(&self) -> String {
+        let contents = fs::read_to_string(&self.path).expect("Error reading file {self.path}");
+        contents
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum SupportedMarkup {
     //Asciidoc,
     Markdown,
-    Text
+    Text,
 }
